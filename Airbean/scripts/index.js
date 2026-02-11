b3a396138b871a334d6e6e126ac8c6c4765ee8cf
Jesper Nyberg <91jesnyb@gafe.molndal.se>
2026-02-12 00:27:10 +0100

    added control

--- Airbean/scripts/index.js
+++ Airbean/scripts/index.js
@@ -2,6 +2,9 @@ import { getMenuItems, setMenuItems } from "./data/menu.js";
 import fetchMenu from "./modules/api.js";
 import { setupBurger, setupCoffeeMenu } from "./modules/gui.js";
 
+console.log(location.pathname);
+
+
 if(location.pathname === '/pages/menu.html') {
     setMenuItems(await fetchMenu());
     setupBurger();
